a07459f20c5cb4507a64e77b65e0eb7ecc8bfad1
yasinadil <adilyasin205@gmail.com>
2023-04-14 22:58:11 +0500

    discover-card-fix

--- components/Discover/Card.tsx
+++ components/Discover/Card.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Verified from "../../public/assets/images/verified.svg";
+import bnbChain from "../../public/assets/images/discover/bnbChain.png";
 export default function Card(props: any) {
   return (
     <div className="card bg-white dark:bg-stone-700 text-black dark:text-white rounded-lg banner-shadow w-max ">
@@ -26,27 +27,41 @@ export default function Card(props: any) {
               className="text-sm font-normal"
               style={{ fontFamily: "VietnamMedium" }}
             >
-              Floor
+              BitKeep Kn...
             </p>{" "}
-            <p
-              className="text-xs text-gray-400 mt-1"
-              style={{ fontFamily: "VietnamMedium", fontSize: "0.75rem" }}
-            >
-              0.21 ETH
-            </p>
+            <Image
+              className=""
+              height={20}
+              width={20}
+              src={bnbChain}
+              alt="item"
+            />
           </div>
-          <div className="flex flex-col justify-end text-left ">
+          <div className="flex flex-col justify-end text-left">
             <p
-              className="text-sm font-normal"
-              style={{ fontFamily: "VietnamMedium" }}
+              className="text-xs mt-1 text-[#10C623] flex"
+              style={{ fontFamily: "VietnamMedium", fontSize: "0.75rem" }}
             >
-              Total Volume
-            </p>{" "}
+              0.000006 BNB
+              <svg
+                className="ml-2"
+                width="10"
+                height="18"
+                viewBox="0 0 10 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M4.60487 11.4859H0.824463L6.11703 0.842209V6.92433H9.89744L4.60487 17.568V11.4859Z"
+                  fill="#10C623"
+                />
+              </svg>
+            </p>
             <p
               className="text-xs text-gray-400 mt-1"
               style={{ fontFamily: "VietnamMedium", fontSize: "0.75rem" }}
             >
-              1.21 ETH
+              Floor 0.00001BNB
             </p>
           </div>
         </div>
