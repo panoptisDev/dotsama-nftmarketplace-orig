ef95f110a54bfc2c2076e611941be4160e600d35
yasinadil <adilyasin205@gmail.com>
2023-08-19 12:51:28 +0500

    useeffect check1

--- components/SwitchNetwork/SwitchNetwork.tsx
+++ components/SwitchNetwork/SwitchNetwork.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import moonbeamLogo from "../../public/assets/images/moonbeam-logo.png";
@@ -22,8 +23,32 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import WalletConnections from "../Connect/ConnectModal";
+import { Button } from "../ui/button";
 import { ModeToggle } from "../ui/ThemeToggle/Toggle";
+import dynamic from "next/dynamic";
+
+const WalletConnections = dynamic(() => import("../Connect/ConnectModal"), {
+  ssr: false,
+  loading: () => (
+    <Button variant="ghost" size="icon">
+      <svg
+        className="h-[1.2rem] w-[1.2rem] md:w-[1.5rem] md:h-[1.5rem]"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        // className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M21 12a2.25 2.25 0 00-2.25-2.25H15a3 3 0 11-6 0H5.25A2.25 2.25 0 003 12m18 0v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 9m18 0V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v3"
+        />
+      </svg>
+    </Button>
+  ),
+});
 
 export default function SwitchNetwork() {
   const { disconnect } = useDisconnect();
